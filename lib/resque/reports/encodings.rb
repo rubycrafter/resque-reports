5c66274068f32921bdcdeab8e1137c51392fb316
Sergey D <sclinede@gmail.com>
2013-09-26 15:01:22 +0600

    fix(resque-reports): fixed dynamic const assignment

--- lib/resque/reports/encodings.rb
+++ lib/resque/reports/encodings.rb
@@ -2,11 +2,12 @@
 module Resque
   module Reports
   	module Encodings
+      module ClassConstants
+        CP1251 = 'w:windows1251'
+        UTF8 = 'utf-8'
+      end
   		def self.included(base)
-  			base.class_eval do 
-          CP1251 = 'w:windows1251'
-          UTF8 = 'utf-8'
-  			end
+  			extend ClassConstants
   		end  		
   	end
   end
